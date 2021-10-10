6985ce25763e9da4f257330e081f3e671aee0489
kadu <kadu999@gmail.com>
2021-10-10 19:48:25 -0300

    signup and login create

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const express = require('express');
 const cors = require('cors');
-const { connect } = require("mongoose");
-const connectDb = require('./config/dbconfig')
+const connectDb = require('./config/dbconfig');
+const authRoutes = require('./routes/auth.routes');
+const authMiddleware = require('./middlewares/auth.middlewares');
+
 
 
 connectDb();
@@ -11,8 +13,19 @@ connectDb();
 
 const app = express();
 
+//middlewares
 app.use(express.json());
 app.use(cors());
+
+
+//Routes
+app.use('/auth', authRoutes);
+
+app.use(authMiddleware);
+
+
+
+
 
 app.listen(process.env.PORT, () => {
     console.log("server running on port:", process.env.PORT);
---
+++ routes/users.routes.js
@@ -0,0 +1,18 @@
+const {Router} = require('express');
+const User = require('../models/User');
+
+const router = Router();
+
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    });
+
+
+
+module.exports = router
